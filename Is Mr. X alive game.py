226e79975d624f505e91a5e6629033c64fd2780b
Suryaansh Chawla <47082682+suryaanshchawla@users.noreply.github.com>
2020-09-05 15:12:44 +0530

    Create Is Mr. X alive game.py
    A fun and simple game created during a pomodora break. It is useful in understanding if-then-else loop. It gives a message to stay safe from Covid-19 which is a pandemic outbroken in 2020.

---
+++ Is Mr. X alive game.py
@@ -0,0 +1,9 @@
+print("Is Mr. X alive?") #Asking the user for input
+x=input()
+if x == "No":
+  print("Om shanti!")
+elif x == "Yes":
+  print("Stay safe from Covid-19")
+else:
+  print("Please write Yes or No") #Telling the user to type either "Yes" or "No"
+  print("Run the code again")
