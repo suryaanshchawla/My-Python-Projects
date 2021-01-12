06856605b58ba44d4c520350721ddf731297d030
Suryaansh Chawla <47082682+suryaanshchawla@users.noreply.github.com>
2021-01-12 17:47:12 +0530

    Made the code efficient

--- Is Mr. X alive game.py
+++ Is Mr. X alive game.py
@@ -1,9 +1,8 @@
-
 x=input("Is Mr. X alive?") #Asking the user for input
-if x == "No":
-  print("Om shanti!")
-elif x == "Yes":
-  print("Stay safe from Covid-19")
-else:
-  print("Please write Yes or No") #Telling the user to type either "Yes" or "No"
-  print("Run the code again")
+try:
+    if x == "No":
+        print("Om shanti!")
+    else:
+        print("Stay safe from Covid-19")
+except:
+    print("Please write Yes or No") #Telling the user to type either "Yes" or "No"
