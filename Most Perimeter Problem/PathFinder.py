6625661c9882f41d3a03992fad7a82c2f7c60a9e
Suryaansh Chawla <47082682+suryaanshchawla@users.noreply.github.com>
2021-01-25 14:23:38 +0530

    Update PathFinder.py

--- Most Perimeter Problem/PathFinder.py
+++ Most Perimeter Problem/PathFinder.py
@@ -1,3 +1,3 @@
 # Algorithm
-# 1. Choose a random path and go til you hit an error.
+# 1. Choose a random path and go until you hit an error.
 # 2. After hitting a barrier go back to the very last path. Then Repeat until we get through the puzzle and also remember the correct path.
