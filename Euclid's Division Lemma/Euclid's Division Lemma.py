153cd1cecffd07b31167a6e2afbca8f756710c8f
Suryaansh Chawla <47082682+suryaanshah@users.noreply.github.com>
2021-03-17 15:26:25 +0530

    Update Euclid's Division Lemma.py

--- Euclid's Division Lemma/Euclid's Division Lemma.py
+++ Euclid's Division Lemma/Euclid's Division Lemma.py
@@ -3,10 +3,36 @@
 
 # Question: for any given pair of integers, find it's q and r.
 
-Ls=input("Enter the integers")
-for a, b in ls:
-    Ans= str(a) +"="+ str((a/b)-a%b)+ str(a%b)
+# Algorithm
+# 1. Take 2 integers
+# 2. See the higher integer.
+# 3. Put the higher one before the '='.
+# 4. then put the smaller after the '='.
+# 5. If they are equal, say that the no. are it's H.C.F.
+# 6. print(larger int = smaller int * some no. + the reaminder)
+# 7. repeat step 6 until r=0
+# 8. when r=0, the devisor is the H.C.F.
 
+def EuclidsDivAlg():
+	try:
+		devident=int(input("Enter the first natural num: "))
+		devisor=int(input("Enter the second natural num: "))
+	except:
+		print("Please enter a natural number")
+		print("For example: 256")
 
-if a<b:
-    print(a+"="+b+"× 0 +"+a)
+	r = devident % devisor
+	q = devident // devisor
+
+	if devident<devisor:
+		devident=devisor
+		devisor=devident
+	elif devident==devisor:
+		print("Both integers are same, so both are the HCF or each other.")
+
+	while r != 0:
+		print(str(step)+"."+str(devident)+"="+str(devisor)+"*"str(devident//devisor)+"+"+str(devident%devisor))
+		if r != 0:
+			devisor=devident
+			r=devisor
+EuclidsDivAlg()
